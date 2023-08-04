ef88dbf442b04a0be22f3691395ddbb766f38578
Snobar Nazir <snobar@gmail.com>
2023-08-04 20:57:24 +0500

    Simple Java Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I am cool");
+               Thread.sleep(2000);
+        }
+    }
+}
